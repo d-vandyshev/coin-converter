52820887eb0ae9a02b5fcb7a354efd32e28cc452
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-02-01 12:55:43 +0500

    feat(model): Add validation to Cryptocurrency

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -1,2 +1,7 @@
 class Cryptocurrency < ApplicationRecord
+
+  validates :name, :symbol, :price_usd, presence: true
+  validates :name, :symbol, length: { maximum: 128 }
+  validates :price_usd, numericality: true
+
 end
