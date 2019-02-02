f144d67afc6470c77c9b18c31e07c6537f0ded26
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-02-02 13:14:04 +0500

    feat(model): Add validation for Conversion

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,5 +1,8 @@
 class Conversion < ApplicationRecord
 
-  belongs_to :from_currency, :class_name => "Cryptocurrency"
-  belongs_to :to_currency,   :class_name => "Cryptocurrency"
+  belongs_to :from_currency, class_name: 'Cryptocurrency'
+  belongs_to :to_currency, class_name: 'Cryptocurrency'
+
+  validate :from_currency_id, :to_currency_id, numericality: {only_integer: true, greater_than: 0}
+  validate :amount, numericality: {greater_than: 0}
 end
