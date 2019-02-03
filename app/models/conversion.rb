7a8050388ea622604c4d31bfc4ed281d00a6958b
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-02-03 23:43:43 +0500

    feat(models): Add options inverse_of and dependent for assoсiations

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,10 +1,9 @@
 class Conversion < ApplicationRecord
+  belongs_to :from_currency, class_name: 'Cryptocurrency', inverse_of: :from_conv_currencies
+  belongs_to :to_currency, class_name: 'Cryptocurrency', inverse_of: :to_conv_currencies
 
-  belongs_to :from_currency, class_name: 'Cryptocurrency'
-  belongs_to :to_currency, class_name: 'Cryptocurrency'
+  validates :from_currency_id, :to_currency_id, numericality: { only_integer: true, greater_than: 0 }
+  validates :amount, numericality: { greater_than: 0 }
 
-  validates :from_currency_id, :to_currency_id, numericality: {only_integer: true, greater_than: 0}
-  validates :amount, numericality: {greater_than: 0}
-
-  scope :list_last, ->(length) {Conversion.includes(:from_currency, :to_currency).last(length)}
+  scope :list_last, ->(length) { Conversion.includes(:from_currency, :to_currency).last(length) }
 end
--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -1,9 +1,17 @@
 class Cryptocurrency < ApplicationRecord
+  has_many :from_conv_currencies,
+           class_name: 'Conversion',
+           foreign_key: 'from_currency_id',
+           dependent: :restrict_with_exception,
+           inverse_of: :from_currency
 
-  has_many :from_currencies, class_name: 'Conversion', foreign_key: 'from_currency_id'
-  has_many :to_currencies, class_name: 'Conversion', foreign_key: 'to_currency_id'
+  has_many :to_conv_currencies,
+           class_name: 'Conversion',
+           foreign_key: 'to_currency_id',
+           dependent: :restrict_with_exception,
+           inverse_of: :to_currency
 
   validates :name, :symbol, :price_usd, presence: true
-  validates :name, :symbol, length: {maximum: 128}
-  validates :price_usd, numericality: {greater_than: 0}
+  validates :name, :symbol, length: { maximum: 128 }
+  validates :price_usd, numericality: { greater_than: 0 }
 end
