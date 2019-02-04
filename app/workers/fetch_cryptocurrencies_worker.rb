05fd314927b33af2ea521c9b6924279b4e649996
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-02-04 06:46:28 +0500

    refactor(worker): Decrease SQL-queries in FetchCryptocurrenciesWorker

--- app/workers/fetch_cryptocurrencies_worker.rb
+++ app/workers/fetch_cryptocurrencies_worker.rb
@@ -4,49 +4,56 @@ class FetchCryptocurrenciesWorker
   include Sidekiq::Worker
 
   def perform
-    sidekiq_retries_exhausted_block do |msg, ex|
+    sidekiq_retries_exhausted_block do |msg, _|
       Sidekiq.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
     end
 
     fetch_cryptocurrencies
-    if Cryptocurrency.first.nil?
-      Cryptocurrency.import @cryptocurrencies
+    @currencies_from_db = Cryptocurrency.all
+    if @currencies_from_db.empty?
+      Cryptocurrency.import @currencies_from_api
       return
     end
     add_new_cryptocurrencies
-    remove_missing_cryptocurrencies
-    update_existing_cryptocurrencies
+    update_exist_cryptocurrencies
   end
 
   private
 
   def fetch_cryptocurrencies
-    c = Rails.configuration.x.api_coinmarketcap
-    connect = Typhoeus::Request.new(c.url, params: c.params, headers: c.headers)
-    connect.run
-    json = JSON.parse(connect.response.body, {symbolize_names: true}) # need to rescue
+    api = Rails.configuration.x.api_coinmarketcap
+    body = request_coinmarketcap(api.url, api.params, api.headers)
 
-    @cryptocurrencies = []
-    json[:data].each do |c|
-      @cryptocurrencies << {name: c[:name], symbol: c[:symbol], price_usd: c.dig(:quote, :USD, :price)}
+    @currencies_from_api = []
+    parse_json_currencies(body).each do |cur|
+      @currencies_from_api << { name: cur[:name], symbol: cur[:symbol], price_usd: cur.dig(:quote, :USD, :price) }
     end
+  end
+
+  def request_coinmarketcap(url, params, headers)
+    connect = Typhoeus::Request.new(url, params: params, headers: headers)
+    connect.run
+    connect.response.body
+  end
+
+  def parse_json_currencies(body)
+    JSON.parse(body, symbolize_names: true).fetch[:data]
   end
 
   def add_new_cryptocurrencies
-    @cryptocurrencies.each do |c|
-      if Cryptocurrency.not_exists? name: c[:name]
-        Cryptocurrency.create(name: c[:name], symbol: c[:symbol], price_usd: c[:price_usd])
-      end
-    end
+    names_db = @currencies_from_db.pluck(:name)
+    Cryptocurrency.import(@currencies_from_api.reject { |cc| names_db.include?(cc) })
   end
 
-  def update_existing_cryptocurrencies
-    @cryptocurrencies.each do |cf|
-      Cryptocurrency.all.each do |cdb|
-        if cdb.name == cf[:name]
-          cdb.update(symbol: cf[:symbol], price_usd: cf[:price_usd])
+  def update_exist_cryptocurrencies
+    for_update = {}
+    @currencies_from_api.each do |api|
+      @currencies_from_db.each do |bd|
+        if bd[:name] == api[:name] && bd[:price_usd] != api[:price_usd]
+          for_update[bd[:id]] = { price_usd: api[:price_usd] }
         end
       end
     end
+    Cryptocurrency.update(for_update.keys, for_update.values)
   end
 end
